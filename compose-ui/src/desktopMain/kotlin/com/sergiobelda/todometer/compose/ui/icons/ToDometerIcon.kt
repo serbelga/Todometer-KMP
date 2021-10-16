8d425ff4f9919110edee53cade73c5f603466fa6
Sergio Belda <serbelga@inf.upv.es>
2021-10-16 18:05:22 +0200

    [desktop] Update ToDometer Home Top bar with icon

--- compose-ui/src/desktopMain/kotlin/com/sergiobelda/todometer/compose/ui/icons/ToDometerIcon.kt
+++ compose-ui/src/desktopMain/kotlin/com/sergiobelda/todometer/compose/ui/icons/ToDometerIcon.kt
@@ -1,12 +1,12 @@
 package com.sergiobelda.todometer.compose.ui.icons
 
-/*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+
 @Composable
 fun iconToDometer() =
     if (MaterialTheme.colors.isLight)
-        painterResource("images/isotype_light.png")
+        painterResource("images/isotype_light.svg")
     else
-        painterResource("images/isotype_dark.png")
-
-
- */
+        painterResource("images/isotype_dark.svg")
