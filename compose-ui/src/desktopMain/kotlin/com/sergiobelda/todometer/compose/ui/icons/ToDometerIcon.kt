3b1c99db13e7f65bcb00293ffdcad75a998f229d
Sergio Belda <serbelga@inf.upv.es>
2021-10-16 15:31:34 +0200

    [desktop] ToDometer Logo

---
+++ compose-ui/src/desktopMain/kotlin/com/sergiobelda/todometer/compose/ui/icons/ToDometerIcon.kt
@@ -0,0 +1,12 @@
+package com.sergiobelda.todometer.compose.ui.icons
+
+/*
+@Composable
+fun iconToDometer() =
+    if (MaterialTheme.colors.isLight)
+        painterResource("images/isotype_light.png")
+    else
+        painterResource("images/isotype_dark.png")
+
+
+ */
