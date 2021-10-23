1e2d75bfeac6ee85105abfa8b19b4bab618ff658
Sergio Belda <serbelga@inf.upv.es>
2021-10-23 02:44:59 +0200

    Upgrade Dependencies: - Compose to 1.1.0-alpha06 - Compose Desktop to 1.0.0-beta1 - Kotlin to 1.5.31

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2021 Sergio Belda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import androidx.compose.animation.Crossfade
-import androidx.compose.desktop.Window
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
 import com.sergiobelda.todometer.common.di.initKoin
 import ui.home.HomeScreen
 import ui.task.AddTaskScreen
@@ -11,19 +32,27 @@ import ui.theme.ToDometerTheme
 
 val koin = initKoin().koin
 
-fun main() = Window(title = "ToDo_meter") {
-    var currentPage by remember { mutableStateOf(Screen.Home) }
-    val navigateToHome: () -> Unit = {
-        currentPage = Screen.Home
-    }
-    val navigateToAddTask: () -> Unit = {
-        currentPage = Screen.AddTask
-    }
-    ToDometerTheme(darkTheme = true) {
-        Crossfade(currentPage) { screen ->
-            when (screen) {
-                Screen.Home -> HomeScreen(navigateToAddTask)
-                Screen.AddTask -> AddTaskScreen(navigateToHome)
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ToDometer",
+        state = WindowState(
+            position = WindowPosition.Aligned(Alignment.Center)
+        )
+    ) {
+        var currentPage: Screen by remember { mutableStateOf(Screen.Home) }
+        val navigateToHome: () -> Unit = {
+            currentPage = Screen.Home
+        }
+        val navigateToAddTask: () -> Unit = {
+            currentPage = Screen.AddTask
+        }
+        ToDometerTheme(darkTheme = isSystemInDarkTheme()) {
+            Crossfade(currentPage) { screen ->
+                when (screen) {
+                    Screen.Home -> HomeScreen(navigateToAddTask)
+                    Screen.AddTask -> AddTaskScreen(navigateToHome)
+                }
             }
         }
     }
