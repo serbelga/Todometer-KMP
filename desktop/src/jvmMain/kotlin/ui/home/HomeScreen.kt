2c313eaf8305f589cb54479307011e57fa237662
Sergio Belda <serbelga@inf.upv.es>
2021-11-12 00:57:27 +0100

    [desktop] WIP HomeScreen: Update UI depending on project and task lists. Add illustrations (no_tasks.svg and no_projects.svg). Removed TodometerTopAppBar.

--- desktop/src/jvmMain/kotlin/ui/home/HomeScreen.kt
+++ desktop/src/jvmMain/kotlin/ui/home/HomeScreen.kt
@@ -17,31 +17,33 @@
 package ui.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.FabPosition
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.twotone.Settings
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -52,7 +54,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import dev.sergiobelda.todometer.common.compose.ui.components.ProjectProgress
 import dev.sergiobelda.todometer.common.compose.ui.project.ProjectListItem
 import dev.sergiobelda.todometer.common.compose.ui.task.TaskItem
 import dev.sergiobelda.todometer.common.compose.ui.theme.TodometerColors
@@ -77,8 +83,8 @@ import ui.icons.iconToDometer
 fun HomeScreen() {
     var addProjectAlertDialogState by remember { mutableStateOf(false) }
     var addTaskAlertDialogState by remember { mutableStateOf(false) }
-
     val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
 
     val setTaskDoingUseCase = koin.get<SetTaskDoingUseCase>()
     val setTaskDoneUseCase = koin.get<SetTaskDoneUseCase>()
@@ -88,18 +94,6 @@ fun HomeScreen() {
     val getProjectsUseCase = koin.get<GetProjectsUseCase>()
     val insertProjectUseCase = koin.get<InsertProjectUseCase>()
     val insertTaskUseCase = koin.get<InsertTaskUseCase>()
-
-    val coroutineScope = rememberCoroutineScope()
-    val setTaskDoing: (String) -> Unit = {
-        coroutineScope.launch {
-            setTaskDoingUseCase(it)
-        }
-    }
-    val setTaskDone: (String) -> Unit = {
-        coroutineScope.launch {
-            setTaskDoneUseCase(it)
-        }
-    }
 
     var projectSelected: Project? by remember { mutableStateOf(null) }
     val projectResultState by getProjectSelectedUseCase().collectAsState(null)
@@ -133,20 +127,27 @@ fun HomeScreen() {
                     }
                 },
                 elevation = 0.dp,
-                backgroundColor = TodometerColors.surface
+                backgroundColor = TodometerColors.surface,
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.TwoTone.Settings, contentDescription = "Settings")
+                    }
+                }
             )
         },
         floatingActionButton = {
-            ExtendedFloatingActionButton(
-                icon = {
-                    Icon(Icons.Rounded.Add, "Add task")
-                },
-                text = {
-                    Text("Add task")
-                },
-                onClick = { addTaskAlertDialogState = true },
-                backgroundColor = TodometerColors.primary
-            )
+            if (projects.isNotEmpty()) {
+                ExtendedFloatingActionButton(
+                    icon = {
+                        Icon(Icons.Rounded.Add, "Add task")
+                    },
+                    text = {
+                        Text("Add task")
+                    },
+                    onClick = { addTaskAlertDialogState = true },
+                    backgroundColor = TodometerColors.primary
+                )
+            }
         },
         floatingActionButtonPosition = FabPosition.End,
         scaffoldState = scaffoldState
@@ -172,53 +173,143 @@ fun HomeScreen() {
                 }
             }
         }
-
-        Row {
-            Column(modifier = Modifier.requiredWidth(256.dp).padding(top = 16.dp)) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "Projects",
-                        style = TodometerTypography.subtitle2
-                    )
-                }
-                Spacer(modifier = Modifier.height(24.dp))
-                LazyColumn {
-                    items(projects) { project ->
-                        ProjectListItem(
-                            project,
-                            project.id == projectSelected?.id
-                        ) {
-                            coroutineScope.launch {
-                                setProjectSelectedUseCase.invoke(it)
-                            }
+        if (projects.isNotEmpty()) {
+            Row {
+                ProjectsNavigationDrawer(
+                    projects,
+                    projectSelected,
+                    onProjectClick = {
+                        coroutineScope.launch {
+                            setProjectSelectedUseCase.invoke(it)
                         }
-                    }
-                }
-                TextButton(
-                    onClick = { addProjectAlertDialogState = true },
-                    modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 8.dp).fillMaxWidth()
-                ) {
-                    Icon(Icons.Rounded.Add, contentDescription = "Add project")
-                    Text(text = "Add project")
-                }
-            }
-            Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
-            Column {
-                Text(text = projectSelected?.name ?: "NOTHING")
-                LazyColumn {
-                    items(tasks) {
-                        TaskItem(
-                            task = it,
-                            onDoingClick = setTaskDoing,
-                            onDoneClick = setTaskDone,
-                            {},
-                            {}
+                    },
+                    onAddProjectClick = { addProjectAlertDialogState = true }
+                )
+                Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
+                Column {
+                    ProjectProgress(projectSelected, tasks)
+                    if (tasks.isEmpty()) {
+                        EmptyTasksListView()
+                    } else {
+                        TasksListView(
+                            tasks,
+                            onDoingClick = {
+                                coroutineScope.launch {
+                                    setTaskDoingUseCase(it)
+                                }
+                            },
+                            onDoneClick = {
+                                coroutineScope.launch {
+                                    setTaskDoneUseCase(it)
+                                }
+                            },
+                            onTaskItemClick = {},
+                            onTaskItemLongClick = {}
                         )
                     }
+                }
+            }
+        } else {
+            EmptyProjectsView(addProject = { addProjectAlertDialogState = true })
+        }
+    }
+}
+
+@Composable
+fun EmptyProjectsView(addProject: () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center).padding(bottom = 72.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource("images/no_projects.svg"),
+                modifier = Modifier.size(240.dp).padding(bottom = 24.dp).background(Color.Gray),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Text("There's any project", modifier = Modifier.padding(bottom = 48.dp))
+            Button(onClick = addProject) {
+                Text("Add project")
+            }
+        }
+    }
+}
+
+@Composable
+fun TasksListView(
+    tasks: List<Task>,
+    onDoingClick: (String) -> Unit,
+    onDoneClick: (String) -> Unit,
+    onTaskItemClick: (String) -> Unit,
+    onTaskItemLongClick: (String) -> Unit
+) {
+    LazyColumn {
+        items(tasks) {
+            TaskItem(
+                task = it,
+                onDoingClick = onDoingClick,
+                onDoneClick = onDoneClick,
+                onClick = onTaskItemClick,
+                onLongClick = onTaskItemLongClick
+            )
+        }
+    }
+}
+
+@Composable
+fun EmptyTasksListView() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center).padding(bottom = 72.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource("images/no_tasks.svg"),
+                modifier = Modifier.size(240.dp).padding(bottom = 24.dp),
+                contentDescription = null
+            )
+            Text("There are any task")
+        }
+    }
+}
+
+@Composable
+fun ProjectsNavigationDrawer(
+    projects: List<Project>,
+    projectSelected: Project?,
+    onProjectClick: (projectId: String) -> Unit,
+    onAddProjectClick: () -> Unit
+) {
+    Column(modifier = Modifier.requiredWidth(280.dp).padding(top = 16.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "PROJECTS",
+                style = TodometerTypography.overline
+            )
+            OutlinedButton(
+                onClick = onAddProjectClick,
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+            ) {
+                Icon(Icons.Rounded.Add, contentDescription = "Add project")
+                Text(text = "Add project")
+            }
+        }
+        LazyColumn(modifier = Modifier.padding(top = 8.dp)) {
+            items(projects) { project ->
+                ProjectListItem(
+                    project,
+                    project.id == projectSelected?.id
+                ) {
+                    onProjectClick(it)
                 }
             }
         }
