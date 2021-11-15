0b85239042673d4e7831291e7cf0d0ab6806fbca
Sergio Belda <serbelga@inf.upv.es>
2021-11-16 00:45:43 +0100

    [android] Update ToDometerTitle

--- android/src/main/java/dev/sergiobelda/todometer/ui/components/ToDometerTitle.kt
+++ android/src/main/java/dev/sergiobelda/todometer/ui/components/ToDometerTitle.kt
@@ -24,7 +24,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import dev.sergiobelda.todometer.R
