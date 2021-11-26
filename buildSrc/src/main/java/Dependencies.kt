b3e7c12dc54f720bbac6949148a9620e4ac88fbb
Sergio Belda <serbelga@inf.upv.es>
2021-11-26 09:46:21 +0100

    Upgrade ktlint version to 0.43.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -27,7 +27,7 @@ object Versions {
     const val androidJunit = "1.1.3"
     const val koin = "3.1.3"
     const val kotlin = "1.5.31"
-    const val ktLint = "0.42.1"
+    const val ktLint = "0.43.0"
     const val ktor = "1.6.5"
     const val ktxVersion = "1.7.0"
     const val lifecycle = "2.4.0"
