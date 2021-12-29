1675ab9c0b18f798393057e6f555a8ee63d8b952
Sergio Belda <serbelga@inf.upv.es>
2021-12-29 20:00:09 +0100

    [wearos] Upgrade version to 1.0.0-dev02

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = Android.wearMinSdk
         targetSdk = Android.targetSdk
         versionCode = 1
-        versionName = "1.0.0-dev01"
+        versionName = "1.0.0-dev02"
     }
 
     buildFeatures {
