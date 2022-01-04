471c5757bd9b6d148a7a77f717d994007d0c8b7f
Sergio Belda <serbelga@inf.upv.es>
2022-01-04 14:33:55 +0100

    [wearos] Update isMinifyEnabled and isShrinkResources

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -28,7 +28,8 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
