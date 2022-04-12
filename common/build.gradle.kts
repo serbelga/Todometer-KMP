0493c496d3309669c092918c6fa2e53666bfb030
Sergio Belda <sbeldagalbis@gmail.com>
2022-04-12 18:05:34 +0200

    [common] Update common to work with iosSimulatorArm64

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -15,14 +15,9 @@ repositories {
 kotlin {
     android()
     jvm("desktop")
-
-    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-            ::iosArm64
-        else
-            ::iosX64
-
-    iosTarget("ios") {}
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     cocoapods {
         summary = "Common"
@@ -31,7 +26,6 @@ kotlin {
         framework {
             baseName = "common"
         }
-        podfile = project.file("../ios/Podfile")
     }
     sourceSets {
         val commonMain by getting {
@@ -68,8 +62,24 @@ kotlin {
                 implementation(libs.mockk.mockk)
             }
         }
-        val iosMain by getting
-        val iosTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
     }
 }
 
