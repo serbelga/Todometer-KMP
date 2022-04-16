415476960f6a4c059f2844b3a676be9c1b0117ad
Sergio Belda <sbeldagalbis@gmail.com>
2022-04-16 18:30:36 +0200

    [common] WIP New Number extension function to format Integers

--- common/src/commonMain/kotlin/dev/sergiobelda/todometer/common/ui/task/TaskDueDate.kt
+++ common/src/commonMain/kotlin/dev/sergiobelda/todometer/common/ui/task/TaskDueDate.kt
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2022 Sergio Belda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.sergiobelda.todometer.common.ui.task
 
+import dev.sergiobelda.todometer.common.extensions.format
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
@@ -21,12 +38,11 @@ object TaskDueDate {
 
         val todayLocalDate = Clock.System.todayAt(timeZone)
 
-        val formattedHour = "${dueDateLocalDateTime.hour}:${dueDateLocalDateTime.minute}h"
+        val formattedHour = "${dueDateLocalDateTime.hour.format(2)}:${dueDateLocalDateTime.minute.format(2)}h"
         return if (dueDateLocalDateTime.date.compareTo(todayLocalDate) == 0) {
             formattedHour
         } else {
-            // TODO: Format day and month 02d.
-            "${dueDateLocalDateTime.dayOfMonth}-${dueDateLocalDateTime.monthNumber}-${dueDateLocalDateTime.year} $formattedHour"
+            "${dueDateLocalDateTime.dayOfMonth.format(2)}-${dueDateLocalDateTime.monthNumber.format(2)}-${dueDateLocalDateTime.year} $formattedHour"
         }
     }
 }
