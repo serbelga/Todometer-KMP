7255fdf3562c2bf3191ca15bd88a82f699d55f4c
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2022-06-28 17:53:21 +0200

    Apply ktlintFormat

--- android/src/main/java/dev/sergiobelda/todometer/ui/addtask/AddTaskScreen.kt
+++ android/src/main/java/dev/sergiobelda/todometer/ui/addtask/AddTaskScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
