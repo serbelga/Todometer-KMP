1d8a2d8534882b8e06036041be26ec18281242c6
Sergio Belda <sbeldagalbis@gmail.com>
2022-06-29 22:00:43 +0200

    [android] Upgrade versionName to 2.2.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "dev.sergiobelda.todometer"
         minSdk = libs.versions.androidMinSdk.get().toInt()
         targetSdk = libs.versions.androidTargetSdk.get().toInt()
-        versionCode = 1220201
-        versionName = "android-2.2.0-beta01"
+        versionCode = 1220400
+        versionName = "android-2.2.0"
     }
 
     buildTypes {
