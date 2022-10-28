7f85fd696e51b5a232bd293d31428bb92af6f9e7
Sergio Belda <sbeldagalbis@gmail.com>
2022-10-28 17:58:00 +0200

    [android] Update versionName to android-2.3.2-rc01

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "dev.sergiobelda.todometer"
         minSdk = libs.versions.androidMinSdk.get().toInt()
         targetSdk = libs.versions.androidTargetSdk.get().toInt()
-        versionCode = 1231400
-        versionName = "android-2.3.1"
+        versionCode = 1232301
+        versionName = "android-2.3.2-rc01"
     }
 
     buildTypes {
