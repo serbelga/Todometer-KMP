0a025a7e132ecfc158ef886d0a6b913a4dd2f9ec
Sergio Belda <sbeldagalbis@gmail.com>
2022-11-15 18:59:40 +0100

    Use DataStore for ios (#160)

--- common/preferences/src/commonMain/kotlin/dev/sergiobelda/todometer/common/preferences/Preferences.kt
+++ common/preferences/src/commonMain/kotlin/dev/sergiobelda/todometer/common/preferences/Preferences.kt
@@ -16,25 +16,71 @@
 
 package dev.sergiobelda.todometer.common.preferences
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import androidx.datastore.preferences.core.Preferences as DataStorePreferences
 
-expect class Preferences {
+class Preferences(private val dataStore: DataStore<DataStorePreferences>) {
 
-    suspend fun set(key: String, value: String)
+    suspend fun set(key: String, value: String) {
+        val stringKey = stringPreferencesKey(key)
+        dataStore.edit { preferences ->
+            preferences[stringKey] = value
+        }
+    }
 
-    suspend fun set(key: String, value: Long)
+    suspend fun set(key: String, value: Long) {
+        val longKey = longPreferencesKey(key)
+        dataStore.edit { preferences ->
+            preferences[longKey] = value
+        }
+    }
 
-    suspend fun set(key: String, value: Int)
+    suspend fun set(key: String, value: Int) {
+        val intKey = intPreferencesKey(key)
+        dataStore.edit { preferences ->
+            preferences[intKey] = value
+        }
+    }
 
-    fun getString(key: String): Flow<String?>
+    fun getString(key: String): Flow<String?> =
+        dataStore.data.map { preferences ->
+            val stringKey = stringPreferencesKey(key)
+            preferences[stringKey]
+        }
 
-    fun getStringOrDefault(key: String, default: String): Flow<String>
+    fun getStringOrDefault(key: String, default: String): Flow<String> =
+        dataStore.data.map { preferences ->
+            val stringKey = stringPreferencesKey(key)
+            preferences[stringKey] ?: default
+        }
 
-    fun getLong(key: String): Flow<Long?>
+    fun getLong(key: String): Flow<Long?> =
+        dataStore.data.map { preferences ->
+            val longKey = longPreferencesKey(key)
+            preferences[longKey]
+        }
 
-    fun getLongOrDefault(key: String, default: Long): Flow<Long>
+    fun getLongOrDefault(key: String, default: Long): Flow<Long> =
+        dataStore.data.map { preferences ->
+            val longKey = longPreferencesKey(key)
+            preferences[longKey] ?: default
+        }
 
-    fun getInt(key: String): Flow<Int?>
+    fun getInt(key: String): Flow<Int?> =
+        dataStore.data.map { preferences ->
+            val intKey = intPreferencesKey(key)
+            preferences[intKey]
+        }
 
-    fun getIntOrDefault(key: String, default: Int): Flow<Int>
+    fun getIntOrDefault(key: String, default: Int): Flow<Int> =
+        dataStore.data.map { preferences ->
+            val intKey = intPreferencesKey(key)
+            preferences[intKey] ?: default
+        }
 }
