d50a0218bce5747a32333a46469ab206fa9403f7
Sergio Belda <sbeldagalbis@gmail.com>
2023-01-22 20:17:15 +0100

    Add copyright header to DependencyGraphGeneratorConventionPlugin

--- build-logic/convention/src/main/kotlin/DependencyGraphGeneratorConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/DependencyGraphGeneratorConventionPlugin.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Sergio Belda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorExtension
 import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorPlugin
 import guru.nidi.graphviz.attribute.Color
@@ -7,7 +23,6 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 class DependencyGraphGeneratorConventionPlugin : Plugin<Project> {
-
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply(DependencyGraphGeneratorPlugin::class.java)
