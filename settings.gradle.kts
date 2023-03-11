60d16bd0ed775bad24750c202e80f568686f6607
Sergio Belda <sbeldagalbis@gmail.com>
2023-03-11 18:01:53 +0100

    Update settings.gradle.kts and ios-compose-ui:main.kt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 dependencyResolutionManagement {
@@ -15,7 +14,6 @@ dependencyResolutionManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "ToDometerMultiplatform"
