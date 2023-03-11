a26b823c2cc139f6c74149c0a1057c2fdff3b972
Sergio Belda <sbeldagalbis@gmail.com>
2023-03-11 16:41:30 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 dependencyResolutionManagement {
