f2f3fc3f063a68a59e0336238afd215a654cc217
Sergio Belda <sbeldagalbis@gmail.com>
2023-03-12 01:50:42 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,14 +6,7 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "ToDometerMultiplatform"
