0bb38ab8f173923d8c6bb5d14484eba8ed8aba45
Sergio Belda <sbeldagalbis@gmail.com>
2023-04-23 16:33:02 +0200

    Update TaskTagIndicator

--- common/compose-ui/src/commonMain/kotlin/dev/sergiobelda/todometer/common/compose/ui/components/TaskTagIndicator.kt
+++ common/compose-ui/src/commonMain/kotlin/dev/sergiobelda/todometer/common/compose/ui/components/TaskTagIndicator.kt
@@ -2,8 +2,10 @@ package dev.sergiobelda.todometer.common.compose.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -15,16 +17,18 @@ import dev.sergiobelda.todometer.common.compose.ui.mapper.composeColorOf
 import dev.sergiobelda.todometer.common.domain.model.Tag
 
 @Composable
-fun TaskTagIndicator(
+fun RowScope.TaskTagIndicator(
     tag: Tag,
     modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
-            .background(color = ToDometerTheme.toDometerColors.composeColorOf(tag))
             .clip(shape = CircleShape)
+            .background(color = ToDometerTheme.toDometerColors.composeColorOf(tag))
             .height(20.dp)
-            .padding(end = 12.dp)
             .width(4.dp)
+    )
+    Spacer(
+        modifier.size(12.dp)
     )
 }
