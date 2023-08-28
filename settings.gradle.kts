e1bb663a26427b493d4fc2f64a62c9113779b42a
Sergio Belda <sbeldagalbis@gmail.com>
2023-08-28 15:25:44 +0200

    Revert applying dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,14 +9,6 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-}
 
 rootProject.name = "ToDometer-KMP"
 
