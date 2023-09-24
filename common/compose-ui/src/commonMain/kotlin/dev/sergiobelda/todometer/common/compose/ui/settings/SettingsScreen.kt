22284b28a14f3a9c5cce31ffd5627b1f00454177
Sergio Belda <sbeldagalbis@gmail.com>
2023-09-24 14:58:55 +0200

    Update SettingsScreen selector

--- common/compose-ui/src/commonMain/kotlin/dev/sergiobelda/todometer/common/compose/ui/settings/SettingsScreen.kt
+++ common/compose-ui/src/commonMain/kotlin/dev/sergiobelda/todometer/common/compose/ui/settings/SettingsScreen.kt
@@ -16,7 +16,13 @@
 
 package dev.sergiobelda.todometer.common.compose.ui.settings
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,9 +30,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -36,9 +44,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
@@ -61,7 +71,7 @@ fun SettingsScreen(
 ) {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
+            TopAppBar(
                 navigationIcon = {
                     IconButton(onClick = navigateBack) {
                         Icon(
@@ -111,35 +121,61 @@ private fun AppThemeItem(
     modifier: Modifier = Modifier
 ) {
     val selectedAlpha = if (selected) Alpha.High else Alpha.Medium
-    Surface(
-        onClick = onClick,
-        shape = RoundedCornerShape(AppThemeItemCornerRadius),
-        border = BorderStroke(
-            width = AppThemeItemBorder,
-            color = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.outline
-        ),
-        modifier = modifier.height(AppThemeItemHeight).fillMaxWidth(),
-        tonalElevation = if (selected) AppThemeItemSelectedElevation else AppThemeItemUnselectedElevation,
-        contentColor = MaterialTheme.colorScheme.onSurface.withAlpha(selectedAlpha)
-    ) {
-        Box {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.align(Alignment.Center)
-            ) {
-                Icon(
-                    appTheme.themeIcon(),
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.height(AppThemeItemSelectedSpacing))
-                Text(
-                    appTheme.themeName(),
-                    textAlign = TextAlign.Center,
-                    maxLines = AppThemeItemMaxLines,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.labelLarge
-                )
+    Box {
+        Surface(
+            onClick = onClick,
+            shape = RoundedCornerShape(AppThemeItemCornerRadius),
+            border = BorderStroke(
+                width = AppThemeItemBorder,
+                color = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.outline
+            ),
+            modifier = modifier
+                .height(AppThemeItemHeight)
+                .fillMaxWidth()
+                .padding(AppThemeItemPadding),
+            tonalElevation = if (selected) AppThemeItemSelectedElevation else AppThemeItemUnselectedElevation,
+            contentColor = MaterialTheme.colorScheme.onSurface.withAlpha(selectedAlpha)
+        ) {
+            Box {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.align(Alignment.Center)
+                ) {
+                    Icon(
+                        appTheme.themeIcon(),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.height(AppThemeItemSelectedSpacing))
+                    Text(
+                        appTheme.themeName(),
+                        textAlign = TextAlign.Center,
+                        maxLines = AppThemeItemMaxLines,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
             }
+        }
+        AnimatedVisibility(
+            visible = selected,
+            enter = scaleIn(
+                animationSpec = tween(
+                    durationMillis = AppThemeItemSelectedIconAnimationDuration,
+                    easing = FastOutSlowInEasing
+                )
+            ),
+            exit = fadeOut()
+        ) {
+            Icon(
+                TodometerIcons.CheckCircle,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.background)
+                    .size(AppThemeItemSelectedIconSize)
+                    .align(Alignment.TopStart)
+            )
         }
     }
 }
@@ -149,11 +185,14 @@ private val SettingsItemInternalSpacing: Dp = 16.dp
 
 private val AppThemeItemsSpacing: Dp = 12.dp
 
-private val AppThemeItemSelectedElevation: Dp = 6.dp
-private val AppThemeItemUnselectedElevation: Dp = 0.dp
-private val AppThemeItemCornerRadius: Dp = 8.dp
-private val AppThemeItemBorder: Dp = 1.dp
-private val AppThemeItemHeight: Dp = 96.dp
-private val AppThemeItemWidth: Dp = 96.dp
-private val AppThemeItemSelectedSpacing: Dp = 6.dp
 private const val AppThemeItemMaxLines: Int = 2
+private val AppThemeItemBorder: Dp = 1.dp
+private val AppThemeItemCornerRadius: Dp = 8.dp
+private val AppThemeItemHeight: Dp = 96.dp
+private val AppThemeItemSelectedElevation: Dp = 6.dp
+private const val AppThemeItemSelectedIconAnimationDuration: Int = 150
+private val AppThemeItemPadding: Dp = 2.dp
+private val AppThemeItemSelectedIconSize: Dp = 20.dp
+private val AppThemeItemSelectedSpacing: Dp = 6.dp
+private val AppThemeItemUnselectedElevation: Dp = 0.dp
+private val AppThemeItemWidth: Dp = 96.dp
