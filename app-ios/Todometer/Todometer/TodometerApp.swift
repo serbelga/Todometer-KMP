1e4557d882420a6d8b7ea1a7969c3e48c1907d22
Sergio Belda <sbeldagalbis@gmail.com>
2023-11-10 09:17:28 +0100

    Create Todometer Xcode project on app-ios module. Update build.gradle.kts. Also, upgrade androidGradlePlugin to 8.2.0-rc03, composeCompiler to 1.5.4, composeMultiplatform to 1.5.10, kotlin to 1.9.20 and ksp to 1.9.20-1.0.14

---
+++ app-ios/Todometer/Todometer/TodometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodometerApp.swift
+//  Todometer
+//
+//  Created by Sergio Belda Galbis on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
