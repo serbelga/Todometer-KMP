058d4de0bdd1d3d02f1039b92f9f8b27569f5029
Sergio Belda <sbeldagalbis@gmail.com>
2023-11-11 15:32:22 +0100

    Remove dependsOn usage in app:common:ui

--- app/common/ui/build.gradle.kts
+++ app/common/ui/build.gradle.kts
@@ -58,24 +58,8 @@ kotlin {
             }
         }
         val desktopTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
+        val iosMain by creating
+        val iosTest by creating
 
         all {
             languageSettings.optIn("kotlin.RequiresOptIn")
