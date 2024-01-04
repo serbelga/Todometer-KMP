27223d2d64fa662ca352e65196811c067b4be612
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2024-01-05 00:30:27 +0100

    Use Compose Vectorize library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.composeVectorize) apply false
     alias(libs.plugins.dependencyGraphGenerator) apply false
     alias(libs.plugins.firebaseCrashlytics) apply false
     alias(libs.plugins.googleServices) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.sqlDelight) apply false
     alias(libs.plugins.spotless)
+    alias(libs.plugins.sqlDelight) apply false
 }
 
 buildscript {
