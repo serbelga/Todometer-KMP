bcef6eb1add7528b5a9784d72dd1e08fbde94567
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2024-02-27 21:29:38 +0100

    Use Compose Vectorize color attribute feature (#484)

--- common/designsystem-resources/src/commonMain/kotlin/dev/sergiobelda/todometer/common/designsystem/resources/images/illustrations/NoTasks.kt
+++
@@ -1,6 +0,0 @@
-package dev.sergiobelda.todometer.common.designsystem.resources.images.illustrations
-
-import androidx.compose.ui.graphics.vector.ImageVector
-import dev.sergiobelda.todometer.common.designsystem.resources.images.Images
-
-expect val Images.Illustrations.NoTasks: ImageVector
