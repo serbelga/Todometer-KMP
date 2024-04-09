d37bba239767dc45d7a837bef3624917de584c06
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2024-04-09 20:40:11 +0200

    Update to Kotlin 2.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://androidx.dev/storage/compose-compiler/repository/")
     }
 }
 
