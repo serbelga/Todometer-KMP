c257222a88ab730495cd625d517c6ab074d9d474
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2025-01-22 15:13:20 +0100

    Refactor plugin names to include group

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.androidTest)
+    alias(libs.plugins.android.test)
     kotlin("android")
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.androidTest) apply false
-    alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.composeVectorize) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.test) apply false
     alias(libs.plugins.dependencyGraphGenerator) apply false
     alias(libs.plugins.detekt) apply false
-    alias(libs.plugins.firebaseCrashlytics) apply false
-    alias(libs.plugins.googleServices) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.google.firebaseCrashlytics) apply false
+    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.jetbrains.composeCompiler) apply false
+    alias(libs.plugins.jetbrains.kotlinMultiplatform) apply false
+    alias(libs.plugins.sergiobelda.composeVectorize) apply false
     alias(libs.plugins.spotless)
     alias(libs.plugins.sqlDelight) apply false
 }
