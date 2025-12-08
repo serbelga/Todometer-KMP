0d8580ea334d8df495c1df9b6d39c7b281848bf6
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2025-12-08 20:46:52 +0100

    Update macrobenchmark to use fake data

--- macrobenchmark/src/prod/kotlin/dev/sergiobelda/todometer/app/macrobenchmark/startup/AppStartupBenchmark.kt
+++ macrobenchmark/src/prod/kotlin/dev/sergiobelda/todometer/app/macrobenchmark/startup/AppStartupBenchmark.kt
@@ -6,6 +6,7 @@ import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.sergiobelda.todometer.app.benchmark.BuildConfig
 import dev.sergiobelda.todometer.app.macrobenchmark.utils.APP_PACKAGE_NAME
 import org.junit.Rule
 import org.junit.Test
@@ -28,7 +29,7 @@ internal class AppStartupBenchmark {
     )
 
     private fun startup(compilationMode: CompilationMode) = macrobenchmarkRule.measureRepeated(
-        packageName = APP_PACKAGE_NAME,
+        packageName = "$APP_PACKAGE_NAME.${BuildConfig.FLAVOR}",
         compilationMode = compilationMode,
         metrics = listOf(StartupTimingMetric()),
         iterations = 5,
