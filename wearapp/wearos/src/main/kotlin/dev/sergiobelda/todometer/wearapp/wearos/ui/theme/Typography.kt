a44594223fa9f095ae47b7191cb5e0aaad75107b
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2026-07-03 20:31:52 +0200

    Update

--- wearapp/wearos/src/main/kotlin/dev/sergiobelda/todometer/wearapp/wearos/ui/theme/Typography.kt
+++ wearapp/wearos/src/main/kotlin/dev/sergiobelda/todometer/wearapp/wearos/ui/theme/Typography.kt
@@ -16,10 +16,11 @@
 
 package dev.sergiobelda.todometer.wearapp.wearos.ui.theme
 
+import androidx.compose.runtime.Composable
 import androidx.wear.compose.material.Typography
 import dev.sergiobelda.todometer.common.designsystem.resources.type.quickSandFontFamily
 
-internal val typography =
+internal val typography @Composable get() =
     Typography(
         defaultFontFamily = quickSandFontFamily,
     )
